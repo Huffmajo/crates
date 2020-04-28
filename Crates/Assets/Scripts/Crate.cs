7b937c94e78e0e59e23727c596b08aa0ba9b3ad1
Huffmajo <huffmajo@oregonstate.edu>
2020-04-28 15:59:56 -0700

    Created swipe angle to parse directional touchs

--- Crates/Assets/Scripts/Crate.cs
+++ Crates/Assets/Scripts/Crate.cs
@@ -6,19 +6,27 @@ public class Crate : MonoBehaviour
 {
 	public Vector3 swipeStartPos;
 	public Vector3 swipeEndPos;
+	public float swipeAngle;
 
 	private LineRenderer swipeLine;
 	private Ray ray;
     private	RaycastHit hit;
+    private Rigidbody rb;
 
+    private enum Direction {UP, DOWN, LEFT, RIGHT};
+    private Direction dir;
 
 	GameObject selectedObj;
 
     void Start()
     {
+    	rb = GetComponent<Rigidbody>();
+
        // instantiate swipe line
        swipeLine = gameObject.AddComponent<LineRenderer>();
        swipeLine.positionCount = 2;
+       swipeLine.startWidth = 1f;
+       swipeLine.endWidth = 0f;
     }
 
     // Update is called once per frame
@@ -71,6 +79,85 @@ public class Crate : MonoBehaviour
         // draw swipe line
         swipeLine.SetPosition(0, swipeStartPos);
    	    swipeLine.SetPosition(1, swipeEndPos);
+   	    Vector3 posDiff = swipeEndPos - swipeStartPos;
+   	    swipeAngle = Mathf.Atan2(posDiff.x, posDiff.z) * Mathf.Rad2Deg + 180;
+    }
+
+    // determines input from swipe
+    /*
+    Direction SwipeInput()
+    {
+
+    }
+	*/
+
+    // check position is clear for movement
+    bool DirectionClear(Direction dir)
+    {
+    	Vector3 dirVector;
+
+    	switch(dir)
+    	{
+    		case Direction.UP:
+    			dirVector = Vector3.forward;
+    			break;
+
+    		case Direction.DOWN:
+    			dirVector = Vector3.back;
+    			break;
+
+    		case Direction.LEFT:
+    			dirVector = Vector3.left;
+    			break;
+
+    		case Direction.RIGHT:
+    			dirVector = Vector3.right;
+    			break;
+
+    		default:
+    			dirVector = Vector3.up;
+    			break;
+    	}
+
+    	if (Physics.Raycast(transform.position, dirVector, out hit, 1f))
+    	{
+    		return false;
+    	}
+    	else
+    	{
+    		return true;
+    	}
+    }
+
+    // move crate
+    void Move(Direction dir, float thrust)
+    {
+    	Vector3 dirVector;
+
+    	switch(dir)
+    	{
+    		case Direction.UP:
+    			dirVector = Vector3.forward;
+    			break;
+
+    		case Direction.DOWN:
+    			dirVector = Vector3.back;
+    			break;
+
+    		case Direction.LEFT:
+    			dirVector = Vector3.left;
+    			break;
+
+    		case Direction.RIGHT:
+    			dirVector = Vector3.right;
+    			break;
+
+    		default:
+    			dirVector = Vector3.up;
+    			break;
+    	}
+
+    	rb.AddForce(dirVector * thrust);
     }
 
 }
