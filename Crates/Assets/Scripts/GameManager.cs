3ae15269acb364993a18f4f72b0b18c2e03050d0
Huffmajo <huffmajo@oregonstate.edu>
2020-05-04 15:55:52 -0700

    Finished test level. GameManager created and basic win state implemented.

---
+++ Crates/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public bool gameWon;
+	public int numMoves;
+	public GameObject targetCrate;
+
+	private RaycastHit hit;
+	private Ray ray;
+
+    void Start()
+    {
+        gameWon = false;
+    }
+
+    void Update()
+    {
+        // check for win condition
+        if (Physics.Raycast(targetCrate.transform.position, Vector3.down , out hit))
+        {
+        	if (hit.collider.gameObject.tag == "Finish" && !gameWon)
+        	{
+        		// change color of crate to show win state
+        		targetCrate.GetComponent<Renderer>().material.color = Color.black;
+        		gameWon = true;
+        	}
+        }
+    }
+}
