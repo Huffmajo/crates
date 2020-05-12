45e324cf55fd25ffc00f68be882a5e9bc74ee313
Joel Huffman <huffmajo@oregonstate.edu>
2020-05-11 22:06:19 -0700

    Updated lighting. Refined Touch Manager script

--- Crates/Assets/Scripts/TouchManager.cs
+++ Crates/Assets/Scripts/TouchManager.cs
@@ -11,9 +11,79 @@ public class TouchManager : MonoBehaviour
     public Vector3 touchEndPos;
 	public GameObject selectedObj;
 
-    void Start()
+    void SlideCrate(GameObject obj)
     {
+    	// move selected object 
+    	if (selectedObj.tag == "Vertical")
+    	{
+    		VerticalCrate script = selectedObj.GetComponent<VerticalCrate>();
 
+    		// sliding up
+            if (touchEndPos.z > selectedObj.transform.position.z)
+            {
+                // don't move past upper limit
+                if (touchEndPos.z > script.zUpperLimit)
+                {
+                    selectedObj.transform.position = new Vector3(selectedObj.transform.position.x, selectedObj.transform.position.y, script.zUpperLimit);
+                }
+                // otherwise follow touch position
+                else
+                {
+                    selectedObj.transform.position = new Vector3(selectedObj.transform.position.x, selectedObj.transform.position.y, Mathf.Round(touchEndPos.z) - 0.5f);
+                }
+            }
+
+            // sliding down
+            if (touchEndPos.z < selectedObj.transform.position.z)
+            {
+                // don't move past upper limit
+                if (touchEndPos.z < script.zLowerLimit)
+                {
+                    selectedObj.transform.position = new Vector3(selectedObj.transform.position.x, selectedObj.transform.position.y, script.zLowerLimit);
+                }
+                // otherwise follow touch position
+                else
+                {
+                    selectedObj.transform.position = new Vector3(selectedObj.transform.position.x, selectedObj.transform.position.y, Mathf.Round(touchEndPos.z) + 0.5f);
+                }
+            }
+    	}
+
+    	// move selected object 
+    	else if (selectedObj.tag == "Horizontal")
+    	{
+    		HorizontalCrate script = selectedObj.GetComponent<HorizontalCrate>();
+
+    		// sliding right
+            if (touchEndPos.x > selectedObj.transform.position.x)
+            {
+                // don't move past right limit
+                if (touchEndPos.x > script.xUpperLimit)
+                {
+                    selectedObj.transform.position = new Vector3(script.xUpperLimit, selectedObj.transform.position.y, selectedObj.transform.position.z);
+                }
+                // otherwise follow touch position
+                else
+                {
+                    selectedObj.transform.position = new Vector3(Mathf.Round(touchEndPos.x) - 0.5f, selectedObj.transform.position.y, selectedObj.transform.position.z);
+                }
+            }
+
+            // sliding left
+            if (touchEndPos.x < selectedObj.transform.position.x)
+            {
+                // don't move past left limit
+                if (touchEndPos.x < script.xLowerLimit)
+                {
+                    selectedObj.transform.position = new Vector3(script.xLowerLimit, selectedObj.transform.position.y, selectedObj.transform.position.z);
+                }
+                // otherwise follow touch position
+                else
+                {
+                    selectedObj.transform.position = new Vector3(Mathf.Round(touchEndPos.x) + 0.5f, selectedObj.transform.position.y, selectedObj.transform.position.z);
+                }
+            }
+    	}
     }
 
     void Update()
@@ -40,77 +110,7 @@ public class TouchManager : MonoBehaviour
         		touchEndPos = hit.point;
         	}
 
-   	    	// move selected object 
-        	if (selectedObj.tag == "Vertical")
-        	{
-        		VerticalCrate script = selectedObj.GetComponent<VerticalCrate>();
-
-        		// sliding up
-                if (touchEndPos.z > selectedObj.transform.position.z)
-                {
-                    // don't move past upper limit
-                    if (touchEndPos.z > script.zUpperLimit)
-                    {
-                        selectedObj.transform.position = new Vector3(selectedObj.transform.position.x, selectedObj.transform.position.y, script.zUpperLimit);
-                    }
-                    // otherwise follow touch position
-                    else
-                    {
-                        selectedObj.transform.position = new Vector3(selectedObj.transform.position.x, selectedObj.transform.position.y, Mathf.Round(touchEndPos.z) - 0.5f);
-                    }
-                }
-
-                // sliding down
-                if (touchEndPos.z < selectedObj.transform.position.z)
-                {
-                    // don't move past upper limit
-                    if (touchEndPos.z < script.zLowerLimit)
-                    {
-                        selectedObj.transform.position = new Vector3(selectedObj.transform.position.x, selectedObj.transform.position.y, script.zLowerLimit);
-                    }
-                    // otherwise follow touch position
-                    else
-                    {
-                        selectedObj.transform.position = new Vector3(selectedObj.transform.position.x, selectedObj.transform.position.y, Mathf.Round(touchEndPos.z) + 0.5f);
-                    }
-                }
-        	}
-
-        	// move selected object 
-        	else if (selectedObj.tag == "Horizontal")
-        	{
-        		HorizontalCrate script = selectedObj.GetComponent<HorizontalCrate>();
-
-        		// sliding right
-                if (touchEndPos.x > selectedObj.transform.position.x)
-                {
-                    // don't move past right limit
-                    if (touchEndPos.x > script.xUpperLimit)
-                    {
-                        selectedObj.transform.position = new Vector3(script.xUpperLimit, selectedObj.transform.position.y, selectedObj.transform.position.z);
-                    }
-                    // otherwise follow touch position
-                    else
-                    {
-                        selectedObj.transform.position = new Vector3(Mathf.Round(touchEndPos.x) - 0.5f, selectedObj.transform.position.y, selectedObj.transform.position.z);
-                    }
-                }
-
-                // sliding left
-                if (touchEndPos.x < selectedObj.transform.position.x)
-                {
-                    // don't move past left limit
-                    if (touchEndPos.x < script.xLowerLimit)
-                    {
-                        selectedObj.transform.position = new Vector3(script.xLowerLimit, selectedObj.transform.position.y, selectedObj.transform.position.z);
-                    }
-                    // otherwise follow touch position
-                    else
-                    {
-                        selectedObj.transform.position = new Vector3(Mathf.Round(touchEndPos.x) + 0.5f, selectedObj.transform.position.y, selectedObj.transform.position.z);
-                    }
-                }
-        	}
+   	    	SlideCrate(selectedObj);
         }
 
         // click/touch release
